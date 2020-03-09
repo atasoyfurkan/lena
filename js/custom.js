6fe6ab413980deb647fb8169440ef7d287e1adbb
muhammed.atasoy <muhammed.atasoy@boun.edu.tr>
2020-03-10 00:08:48 +0300

    filter

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,48 @@
+function filter(c) {
+  var x, i;
+  x = document.getElementsByClassName("product-label");
+  if (c == "all") c = "";
+  // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
+  for (i = 0; i < x.length; i++) {
+    addClass(x[i], "d-none");
+    if (x[i].className.indexOf(c) > -1) removeClass(x[i], "d-none");
+  }
+}
+// Show filtered elements
+function addClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {
+      element.className += " " + arr2[i];
+    }
+  }
+}
+// Hide elements that are not selected
+function removeClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);
+    }
+  }
+  element.className = arr1.join(" ");
+}
+var btnContainer = document.getElementById("filter-buttons");
+if (btnContainer) {
+  var btns = btnContainer.getElementsByClassName("btn");
+  for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function() {
+      var current = document.getElementsByClassName("btn-active");
+      current[0].className = current[0].className.replace(" btn-active", "");
+      this.className += " btn-active";
+    });
+  }
+}
+
 (function($) {
   // 'use strict';
 
@@ -16,6 +61,8 @@
     $(".nav-bar").addClass("nav-bar-scrolled");
     $(".nav-bar").addClass("shadow-lg");
   }
+
+  // Products
 
   // Main Navigation
   $(".hamburger-menu").on("click", function() {
