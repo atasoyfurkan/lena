d225c143ecc70576ea97b3ada8eb25b1caf90d81
atasoyfurkan <furkanatasoy65@gmail.com>
2020-03-26 22:01:54 +0300

    deneme5

--- send-email.php
+++ send-email.php
@@ -1,5 +1,17 @@
 <?php
-echo mail("furkanatasoy65@gmail.com", "deneme", "deneme123333", "From: webmaster@example.com\r\n");
+$header = "From: noreply@example.com\r\n";
+$header.= "MIME-Version: 1.0\r\n";
+$header.= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+$header.= "X-Priority: 1\r\n";
+
+$status = mail("furkanatasoy65@gmail.com", "deneme", "deneme123333", $header);
+
+if($status)
+{
+    echo '<p>Your mail has been sent!</p>';
+} else {
+    echo '<p>Something went wrong. Please try again!</p>';
+}
 ?>
  
 <!-- include your own success html here -->
